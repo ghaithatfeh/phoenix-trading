882d4c60e6134eb2ae10f2fd8cf4bdd80ab209c7
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2025-09-30 23:14:22 +0300

    Integrate Segoe UI Variable as the primary font and add Tailwind CSS custom theme with updated color and font utilities

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { FacebookIcon, Instagram, Mail, Phone } from "lucide-react";
 function App() {
   return (
     <>
-      <div className={"relative h-screen w-full bg-black"}>
+      <div className={"bg-dark relative h-screen w-full"}>
         {/* Navbar */}
         <div className={"flex items-center justify-center py-10"}>
           <nav
@@ -13,19 +13,19 @@ function App() {
           >
             <a
               href={"/"}
-              className={`${window.location.pathname == "/" ? "!text-red-700" : ""} hover:text-red-400`}
+              className={`${window.location.pathname == "/" ? "!text-primary" : ""} hover:text-red-400`}
             >
               Home
             </a>
             <a
               href={"/our-products"}
-              className={`${window.location.pathname == "/our-products" ? "!text-red-700" : ""} hover:text-red-400`}
+              className={`${window.location.pathname == "/our-products" ? "!text-primary" : ""} hover:text-red-400`}
             >
               Our Products
             </a>
             <a
               href={"/contact-us"}
-              className={`${window.location.pathname == "/contact-us" ? "!text-red-700" : ""} hover:text-red-400`}
+              className={`${window.location.pathname == "/contact-us" ? "!text-primary" : ""} hover:text-red-400`}
             >
               Contact Us
             </a>
@@ -39,7 +39,7 @@ function App() {
 
         <div className={"flex w-full items-center justify-between"}>
           <div className={"w-[60%]"}>
-            <h1 className={"ps-26 text-6xl font-bold text-red-700"}>
+            <h1 className={"ps-26 text-6xl font-bold text-primary font-segoe"}>
               Welcome to Phoenix
             </h1>
             <p className={"my-10 ps-26 text-3xl text-white"}>
@@ -50,9 +50,9 @@ function App() {
 
             <div className={"flex max-h-[20vh] items-center justify-between"}>
               <div className="flex flex-col space-y-6">
-                <div className="h-2 w-20 rounded-r-full bg-gradient-to-r from-red-700 to-red-600"></div>
-                <div className="h-2 w-20 rounded-r-full bg-gradient-to-r from-red-700 to-red-600"></div>
-                <div className="h-2 w-20 rounded-r-full bg-gradient-to-r from-red-700 to-red-600"></div>
+                <div className="h-2 w-20 rounded-r-full bg-gradient-to-r from-primary to-red-600"></div>
+                <div className="h-2 w-20 rounded-r-full bg-gradient-to-r from-primary to-red-600"></div>
+                <div className="h-2 w-20 rounded-r-full bg-gradient-to-r from-primary to-red-600"></div>
               </div>
               <img
                 src={"/Logo-Phoenix.svg"}
@@ -79,7 +79,7 @@ function App() {
           </div>
         </div>
       </div>
-      <div className="relative h-[35vh] w-full bg-red-600/80">
+      <div className="relative h-[35vh] w-full bg-primary">
         <img
           src="/05-Market-BG.jpg"
           alt="Background"
@@ -94,9 +94,9 @@ function App() {
           </p>
         </div>
       </div>
-      <div className={"w-full bg-black"}>
+      <div className={"bg-dark w-full"}>
         <div className={"flex w-full items-center justify-center py-10"}>
-          <h1 className={"text-5xl font-bold text-red-700"}>
+          <h1 className={"text-5xl font-bold text-primary"}>
             Our Journey & Global Reach
           </h1>
         </div>
@@ -120,16 +120,16 @@ function App() {
         </div>
 
         <div className={"flex w-full items-center justify-center py-10"}>
-          <h1 className={"text-5xl font-bold text-red-700"}>
+          <h1 className={"text-5xl font-bold text-primary"}>
             Why Choose Arizona Food?
           </h1>
         </div>
         <div className={"flex flex-col items-start space-y-16 px-26"}>
           <div className={"flex items-center justify-between gap-2"}>
-            <div className={"h-full border-r-2 border-r-red-800 py-4 pe-2"}>
+            <div className={"h-full border-r-2 border-r-red-800 py-4 pe-5"}>
               <img src={"/07-icon.svg"} className={"w-20"} />
             </div>
-            <div className={"text-start"}>
+            <div className={"text-start ps-3"}>
               <h2 className={"text-3xl font-bold text-white"}>
                 Global Quality Standards:
               </h2>
@@ -142,10 +142,10 @@ function App() {
           </div>
 
           <div className={"flex items-center justify-between gap-2"}>
-            <div className={"h-full border-r-2 border-r-red-800 py-4 pe-2"}>
+            <div className={"h-full border-r-2 border-r-red-800 py-4 pe-5"}>
               <img src={"/08-icon.svg"} className={"w-13"} />
             </div>
-            <div className={"text-start"}>
+            <div className={"text-start ps-3"}>
               <h2 className={"text-3xl font-bold text-white"}>
                 Diverse Product Range:
               </h2>
@@ -157,10 +157,10 @@ function App() {
           </div>
 
           <div className={"flex items-center justify-between gap-2"}>
-            <div className={"h-full border-r-2 border-r-red-800 py-4 pe-2"}>
+            <div className={"h-full border-r-2 border-r-red-800 py-4 pe-5"}>
               <img src={"/09-icon.svg"} className={"w-15"} />
             </div>
-            <div className={"text-start"}>
+            <div className={"text-start ps-3"}>
               <h2 className={"text-3xl font-bold text-white"}>
                 Reliable Partnership:
               </h2>
@@ -173,10 +173,10 @@ function App() {
           </div>
 
           <div className={"flex items-center justify-between gap-2"}>
-            <div className={"h-full border-r-2 border-r-red-800 py-4 pe-2"}>
+            <div className={"h-full border-r-2 border-r-red-800 py-4 pe-5"}>
               <img src={"/10-icon.svg"} className={"w-13"} />
             </div>
-            <div className={"text-start"}>
+            <div className={"text-start ps-3"}>
               <h2 className={"text-3xl font-bold text-white"}>
                 Commitment to Innovation:
               </h2>
@@ -193,7 +193,7 @@ function App() {
         </div>
 
         <div className={"px-26 pb-10"}>
-          <h1 className={"mb-10 text-start text-3xl font-bold text-red-700"}>
+          <h1 className={"mb-10 text-start text-3xl font-bold text-primary"}>
             Phoenix General Trading Inc
           </h1>
           <div className={"flex w-full items-center justify-between"}>
@@ -202,19 +202,19 @@ function App() {
             >
               <div className={"flex items-center gap-2 text-2xl text-white"}>
                 <img src={"/canada.png"} className={"w-8"} />
-                <Phone className={"fill-red-700 text-red-700"} />
+                <Phone className={"fill-primary text-primary"} />
                 +1 (437) 231-1444
               </div>
 
               <div className={"flex items-center gap-2 text-2xl text-white"}>
                 <img src={"/turkey.png"} className={"w-8"} />
-                <Phone className={"fill-red-700 text-red-700"} />
+                <Phone className={"fill-primary text-primary"} />
                 +90 539 693 11 33
               </div>
 
               <div className={"flex items-center gap-2 text-2xl text-white"}>
                 <img src={"/syria.png"} className={"w-8"} />
-                <Phone className={"fill-red-700 text-red-700"} />
+                <Phone className={"fill-primary text-primary"} />
                 +963 937 777 960
               </div>
             </div>
@@ -222,16 +222,16 @@ function App() {
               className={"flex flex-col items-start justify-start space-y-6"}
             >
               <div className={"flex items-center gap-2 text-2xl text-white"}>
-                <Mail className={"text-red-700"} />
+                <Mail className={"text-primary"} />
                 Info@phoenixtradings.ca
               </div>
 
               <div className={"flex items-center gap-2 text-2xl text-white"}>
-                <Instagram className={"text-red-700"} />
+                <Instagram className={"text-primary"} />
                 ARIZONA1FOODS
               </div>
               <div className={"flex items-center gap-2 text-2xl text-white"}>
-                <FacebookIcon className={"text-red-700"} />
+                <FacebookIcon className={"text-primary"} />
                 Arizonafoods
               </div>
             </div>
